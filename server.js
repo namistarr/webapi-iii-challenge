cdbe787dba0d47f5af61a8ed1981e36a31650c79
namistarr <namistarr@icloud.com>
2019-09-04 16:07:43 -0700

    adding CRUD endpoints

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  const date = new Date().toLocaleDateString();
+  const time = new Date().toLocaleTimeString();
 
-};
+  console.log(`${req.method} Request | http://localhost:8000${req.url} | ${date}, @ ${time}`)
+  next();
+}
+
+
 
 module.exports = server;
